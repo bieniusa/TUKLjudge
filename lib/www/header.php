a0bb5a06fdc5cfcf69ed527e83ab7d1f304efa34
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2017-03-28 20:15:49 +0200

    Update header.php

--- lib/www/header.php
+++ lib/www/header.php
@@ -91,47 +91,6 @@ if ( IS_JURY ) {
         <span class="icon-bar"></span>
       </button>
     </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li class="dropdown brand">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-            <span class="tumjudge-instance tumjudge-info"><span class="pagetitle">ICPC@TUM</span> Info</span>
-            <span class="tumjudge-instance tumjudge-main"><span class="pagetitle">TUMjudge</span> Main</span>
-            <span class="tumjudge-instance tumjudge-test"><span class="pagetitle">TUMjudge</span> Test</span>
-            <span class="tumjudge-instance tumjudge-conpra"><span class="pagetitle">TUMjudge</span> ConPra</span>
-            <span class="tumjudge-instance tumjudge-contest"><span class="pagetitle">TUMjudge</span> Contest</span>
-            <span class="tumjudge-instance tumjudge-gad"><span class="pagetitle">TUMjudge</span> GAD</span>
-            <span class="tumjudge-instance tumjudge-isabelle"><span class="pagetitle">TUMjudge</span> Isabelle</span>
-            <span class="tumjudge-instance tumjudge-gcpc"><span class="pagetitle">TUMjudge</span> GCPC</span>
-            <span class="tumjudge-instance tumjudge-ioide"><span class="pagetitle">TUMjudge</span> IOI Germany</span>
-            <span class="tumjudge-instance tumjudge-ioiat"><span class="pagetitle">TUMjudge</span> IOI Austria</span>
-            <span class="tumjudge-instance tumjudge-challenge"><span class="pagetitle">TUMjudge</span> Challenge</span>
-            <span class="tumjudge-instance tumjudge-mlr"><span class="pagetitle">TUMjudge</span> MLR</span>
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a class="pagetitle">ICPC@TUM</a></li>
-            <?php if(IS_JURY): ?>
-              <li class="page tumjudge-git"><a href="https://git-judge.in.tum.de">GIT <span class="description">Source Code Management</span></a></li>
-            <?php endif; ?>
-            <li class="page tumjudge-info"><a href="https://icpc.tum.de">Website <span class="description">Information about the ICPC</span></a></li>
-            <li class="page tumjudge-discuss"><a href="https://judge.in.tum.de/discuss/">Discuss <span class="description">Discuss all about TUMjudge and ICPC</span></a></li>
-            <li><a class="pagetitle">TUMjudge</a></li>
-            <?php if(IS_JURY): ?>
-              <li class="page tumjudge-main"><a href="https://judge.in.tum.de/main/">Main <span class="description">Account Management</span></a></li>
-              <li class="page tumjudge-test"><a href="https://judge.in.tum.de/test/">Test <span class="description">Testing for TUMjudge Admins</span></a></li>
-            <?php endif; ?>
-            <li class="page tumjudge-conpra"><a href="https://judge.in.tum.de/conpra/">ConPra <span class="description">Lecture &ldquo;Algorithms for Programming Contests&rdquo;</span></a></li>
-            <li class="page tumjudge-contest"><a href="https://judge.in.tum.de/contest/">Contest <span class="description">Preparation for the ACM ICPC</span></a></li>
-            <li class="page tumjudge-gad"><a href="https://judge.in.tum.de/gad/">GAD <span class="description">Lecture &ldquo;Foundations: Algorithms and Data Structures&rdquo;</span></a></li>
-            <li class="page tumjudge-isabelle"><a href="https://judge.in.tum.de/isabelle/">Isabelle <span class="description">Proving Contests</span></a></li>
-            <li class="page tumjudge-gcpc"><a href="https://judge.in.tum.de/gcpc/">GCPC <span class="description">German Collegiate Programming Contest</span></a></li>
-            <li class="page tumjudge-ioide"><a href="https://judge.in.tum.de/ioide/">IOI Germany <span class="description">Preparation for the IOI</span></a></li>
-            <li class="page tumjudge-ioiat"><a href="https://judge.in.tum.de/ioiat/">IOI Austria <span class="description">Preparation for the IOI</span></a></li>
-            <li class="page tumjudge-challenge"><a href="https://judge.in.tum.de/challenge/">Challenge <span class="description">Yet Another Programming Contest</span></a></li>
-            <li class="page tumjudge-mlr"><a href="https://judge.in.tum.de/mlr/">MLR <span class="description">Lecture &ldquo;Machine Learning in Robotics&rdquo;</span></a></li>
-          </ul>
-        </li>
       <?php
         /* NOTE: here a local menu.php is included
          *       both jury and team have their own menu.php
